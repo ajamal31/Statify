23e8726807da0fc4138b64a56bed38cf165c15e4
jonesj38 <jonesj38@mymacewan.ca>
2017-04-02 05:23:32 -0600

    sunburst is done

--- src/app/controllers/statify_controller.js
+++ src/app/controllers/statify_controller.js
@@ -1,7 +1,3 @@
 exports.home = function(req, res){
   res.render(__dirname + '/../../public/views/home.ejs')
 };
-
-exports.sunburst = function(req, res){
-    res.render(__dirname + '/../../public/views/sunburst.ejs')
-};
