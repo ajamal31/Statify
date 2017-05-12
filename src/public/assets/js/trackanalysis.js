c01514d58ec19be695257ada11733c2934d0f4cb
Alan Jamal <AlanJamal@Alans-MacBook-Pro.local>
2017-05-12 01:06:49 -0600

    Fix navigation bug, add animations

--- src/public/assets/js/trackanalysis.js
+++ src/public/assets/js/trackanalysis.js
@@ -1,12 +1,14 @@
+// Event listeners for the track analysis
+
 $(document).ready(function() {
 
     $('.track-img').hover(
-            function() {
-                $('#analysis' + $(this).attr('track-id')).show(300);
-            },
-            function() {
-                $('#analysis' + $(this).attr('track-id')).hide(300);
-            });
+        function() {
+            $('#analysis' + $(this).attr('track-id')).show(300);
+        },
+        function() {
+            $('#analysis' + $(this).attr('track-id')).hide(300);
+        });
 
     $('.track-row').on('click', function() {
         $('#analysis' + $(this).attr('track-row-id')).toggle(300);
