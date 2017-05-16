ae01ced44ac35fa7c5c6b3fdc85cdb9d1419c108
Arun Jamal <jamala6@mymacewan.ca>
2017-05-15 21:47:06 -0600

    Remove mouseover feature in song analysis

--- src/public/assets/js/trackanalysis.js
+++ src/public/assets/js/trackanalysis.js
@@ -2,16 +2,8 @@
 
 $(document).ready(function() {
 
-    $('.track-img').hover(
-        function() {
-            $('#analysis' + $(this).attr('track-id')).show(300);
-        },
-        function() {
-            $('#analysis' + $(this).attr('track-id')).hide(300);
-        });
-
-    $('.track-row').on('click', function() {
-        $('#analysis' + $(this).attr('track-row-id')).toggle(300);
+    $('.track-img').on('click', function() {
+        $('#analysis' + $(this).attr('track-id')).toggle(300);
     });
 
     $('#show-analysis').on('click', function() {
